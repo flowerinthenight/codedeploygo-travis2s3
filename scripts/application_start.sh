273bddf04655c93404715b1f55c92c9b346e6834
flowerinthenight <chew.esmero@mobingi.com>
2017-08-25 15:38:12 +0900

    Test codedeploy update.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# usually sudo but not needed because appspec.yml has runas root
-# service codedeploy start
-# cd /home/ubuntu/golang/src/github.com/flowerinthenight/codedeploy-golang/
-
-# restore dependencies
-# godep restore
-
-# run server
 cd /home/ubuntu/codedeploygo-travis2s3/
 ./codedeploy-golang > /dev/null 2> /dev/null < /dev/null &
-exit 0
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# service codedeploy stop
 killall -v codedeploygo-travis2s3 > /dev/null 2>&1
-exit 0
